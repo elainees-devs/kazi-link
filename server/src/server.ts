9f0b31101cbf3d9c502b6071b330d672ebd85774
elainees-devs <elainees.devs@gmail.com>
2026-04-06 14:26:26 +0300

    chore: update port number

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(cors());
 app.use(express.json());
