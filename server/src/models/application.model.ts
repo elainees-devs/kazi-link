397aeb2b173ec9dc14e40ce50cc89e5a5bd4e42f
elainees-devs <elainees.devs@gmail.com>
2026-04-07 19:56:49 +0300

    feat(application): add employerEmail and emailStatus fields

--- server/src/models/application.model.ts
+++ server/src/models/application.model.ts
@@ -1,5 +1,6 @@
 import mongoose, { Schema, Document } from 'mongoose';
 import { IApplication } from '../types/interfaces.types';
+import { EmailStatus } from '../constants/enums';
 
 export interface ApplicationDocument extends IApplication, Document {}
 
@@ -9,6 +10,13 @@ const ApplicationSchema = new Schema<ApplicationDocument>({
   applicantEmail: { type: String, required: true },
   coverLetter: { type: String },
   resumeUrl: { type: String, required: true },
+  employerEmail: { type: String, required: true },
+  emailStatus: {
+    type: String,
+    enum: Object.values(EmailStatus),
+    default: EmailStatus.Pending,
+  },
+  appliedAt: { type: Date, default: Date.now },
 })
 
 export const ApplicationModel = mongoose.model<ApplicationDocument>('Application', ApplicationSchema);
