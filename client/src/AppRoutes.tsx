dacf4d5d54141975900b89907982af4e397d2e07
elainees-devs <elainees.devs@gmail.com>
2026-04-07 11:47:47 +0300

    feat: add application routes using react-router-dom

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -1,1 +1,16 @@
-// AppRoutes.tsx - React Router setup
+
+import { Routes, Route } from 'react-router-dom';
+import { Apply, Home, JobDetails } from './pages';
+import Jobs from './pages/Jobs';
+
+const AppRoutes = () => (
+	<Routes>
+		<Route path="/" element={<Home />} />
+		<Route path="/jobs" element={<Jobs />} />
+		<Route path="/jobs/:id" element={<JobDetails />} />
+		<Route path="/apply" element={<Apply />} />
+		{/* Add more routes as needed */}
+	</Routes>
+);
+
+export default AppRoutes;
