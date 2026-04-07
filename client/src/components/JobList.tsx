5e20d2d2708a5896d844a8a274f3f53286f4a6df
elainees-devs <elainees.devs@gmail.com>
2026-04-07 11:50:33 +0300

    feat: add JobList component to display job cards grid

--- client/src/components/JobList.tsx
+++ client/src/components/JobList.tsx
@@ -1,1 +1,23 @@
-// JobList.tsx - Renders list of jobs
+
+import React from 'react';
+import type { IJob } from '../types/interfaces.types';
+import JobCard from './JobCard';
+
+interface JobListProps {
+	jobs: IJob[];
+}
+
+const JobList: React.FC<JobListProps> = ({ jobs }) => {
+	if (!jobs.length) {
+		return <div className="text-gray-500">No jobs found.</div>;
+	}
+	return (
+		<div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+			{jobs.map((job) => (
+				<JobCard key={job.id} job={job} />
+			))}
+		</div>
+	);
+};
+
+export default JobList;
