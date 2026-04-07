f333e52c4c7449ea3c38d859d10f4d146eebbd0f
elainees-devs <elainees.devs@gmail.com>
2026-04-07 11:51:16 +0300

    feat: add Loader component for loading states

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -1,1 +1,12 @@
-// Loader.tsx - Loading spinner
+
+import React from 'react';
+import { FaSpinner } from 'react-icons/fa';
+
+const Loader: React.FC = () => (
+	<div className="flex justify-center items-center py-8">
+		<FaSpinner className="animate-spin text-blue-600" size={40} />
+		<span className="ml-3 text-blue-600 font-medium">Loading...</span>
+	</div>
+);
+
+export default Loader;
