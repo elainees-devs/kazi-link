d3193a477a6c9fe1f27dfa18a9de44dd78961b72
elainees-devs <elainees.devs@gmail.com>
2026-04-07 11:48:54 +0300

    feat: add reusable ErrorMessage component

--- client/src/components/ErrorMessage.tsx
+++ client/src/components/ErrorMessage.tsx
@@ -1,1 +1,10 @@
 // ErrorMessage.tsx - Error message display
+const ErrorMessage: React.FC<{ message: string }> = ({ message }) => {
+  return (
+    <div className="error-message">
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default ErrorMessage;
