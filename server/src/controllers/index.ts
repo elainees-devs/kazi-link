ed30d2c8bf60a9f112335503ec366cbf015edf0d
elainees-devs <elainees.devs@gmail.com>
2026-04-08 20:05:21 +0300

    chore(controllers): consolidate exports for category, job, application, analytics, and subCategory controllers

---
+++ server/src/controllers/index.ts
@@ -0,0 +1,5 @@
+export * from "./category.controller";
+export * from "./job.controller";
+export * from "./application.controller";
+export * from "./analytics.controller";
+export * from "./subCategory.controller";
